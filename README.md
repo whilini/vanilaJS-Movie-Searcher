68b1a78290d8e4b3d0a6278fec9e6eb289eb3d84
whilini <whi0306@naver.com>
2022-05-18 19:58:15 +0900

    feat: finish

---
+++ README.md
@@ -0,0 +1,236 @@
+# 🎥 영화 검색 프로젝트
+
+- 과제 기한:
+  - 과제 수행 기간: 04월 28일(목) ~ 05월 19일(목)
+  - 코드 리뷰 기간: 05월 19일(목) ~ 05월 27일(금)
+- 내용:
+  - 주어진 API를 활용해 영화 검색 프로젝트를 만들어보세요.
+
+## 요구사항
+
+### 필수 요구사항
+
+- [x] 검색어를 입력해 영화를 검색할 수 있어야 합니다.
+- [x] 검색된 결과(영화 목록)을 출력해야 합니다.
+- [x] 프론트엔드 프레임워크 없이 바닐라 자바스크립트만으로 개발해야 합니다.
+- [x] 실제 서비스로 배포하고 접근 가능한 링크를 추가해야 합니다.
+
+### 선택 요구사항
+
+- [x] Webpack 프로젝트로 구성해보세요.
+- [ ] 클라이언트에서 API Key가 노출되지 않도록 만들어보세요.
+- [x] 무한 스크롤을 위한 'Intersection Observer'를 활용해보세요.
+- [ ] 최초 API 요청(Request)에 대한 로딩 애니메이션을 추가해보세요.
+- [x] SCSS, Bootstrap 등을 구성해 프로젝트를 최대한 예쁘게(?) 만들어보세요.
+- [x] 영화 포스터 주소에 포함된 `SX300`를 `SX700`과 같이 더 큰 숫자로 수정해 요청하세요.
+- [ ] 실시간으로 이미지의 크기를 다르게 요청하는 것이 어떤 원리로 가능한지 조사해보세요.
+- [ ] 요청 주소에 HTTP가 아닌 HTTPS 프로토콜을 사용해야 하는 이유를 조사해보세요.
+
+###README
+일반적인 해상도의 비율에서 보시는 것을 추천드립니다. 저는 16:9 비율의 모니터로 작업하였습니다.
+
+<html>
+<body>
+<!--StartFragment-->
+
+| 화면비율 | 주요 해상도                                                                         |
+| -------- | ----------------------------------------------------------------------------------- |
+| 4:3      | 1400x1050, 1440x1080, 1600x1200, 1920x1440, 2048x1536                               |
+| 16:10    | 1280x800, 1920x1200, 2560x1600                                                      |
+| 16:9     | 1280x720, 1366x768, 1600x900, 1920x1080, 2560x1440, 3840x2160, 5120x2880, 7680x4320 |
+
+<!--EndFragment-->
+</body>
+</html>
+
+---
+
+## 💡Custom
+
+**💡초기 화면에서 보이는 검색창 화면과 무언갈 검색한 이후 검색창 화면을 전환하기 위해 form 내의 버튼의 submit이벤트 기준으로 javascript로 scss 파일 변경**
+
+⚠️아무런 값을 입력하지 않았을 때는 화면이 전환되지 않도록 막아야 함
+
+```jsx
+if (headerClass.contains("init") && searchInput.value != "") {
+  // 실행할 코드
+}
+```
+
+<br>
+
+**💡검색 결과 개수가 위에 뜨도록 함**
+
+```jsx
+totalSearch.innerHTML = `" <strong>${title}</strong> "의 검색결과: ${totalSearchData}개`;
+```
+
+<br>
+
+**💡평점의 정보를 이용해 별점을 만들어 표시함**
+
+평점으로 노란 별의 `width`를 %로 조절함
+
+```jsx
+detailRateStar.style.width = `calc(${data.imdbRating} * 10%)`;
+```
+
+<br>
+
+**💡포스터 사진을 고화질로 가져옴**
+
+```jsx
+const data = await getMovies(title, pageNo);
+const movieCard = data.Search.map((d) => {
+  const bigPoster = d.Poster.replace("SX300", "SX700");
+  return createCard(d.Title, d.Year, bigPoster);
+});
+```
+
+<br>
+
+**💡OMBd API 문서를 보니 장르와 줄거리, 평점, 런타임 등 다양한 정보를 가져올 수 있음**
+
+이를 이용하여 상세보기 페이지도 만들어보기로 함
+많은 정보가 있진 않아서 상세보기를 새로운 페이지로 넘어가기보다 모달로 띄우도록 함
+
+<br>
+
+**💡API key를 숨김:webpack 환경변수 `.env` 파일 생성**
+
+<br>
+
+💡**이벤트리스너의 최적화를 위해 카드 하나가 아닌 카드리스트를 클릭했을 때 이벤트가 일어나게 함**
+
+⚠️이벤트가 일어난 카드 한장의 정보만 필요한데 `((e) ⇒ e.target.id)`를 했더니 이벤트 버블링으로 인해 정확한 이벤트 타겟이 잡히지 않음
+
+```jsx
+const movieCard = e.target.closest(".movie-card");
+// 영화 카드가 아니라면 아무 이벤트 일어나지 않음
+if (!movieCard) return;
+
+// 카드 리스트 중 영화카드가 아니라면 아무 이벤트 일어나지 않음
+if (!movieCards.contains(movieCard)) return;
+```
+
+<br>
+
+💡**검색결과가 없을 때 보이는 화면을 만듬**
+
+```jsx
+if (data.Response === "False") {
+  totalSearch.innerHTML = `" <strong>${title}</strong> "의 검색결과: 0개`;
+  cards.innerHTML =
+    "<img class='no-result' src='image/no-results.png' alt='no-results'>";
+  return;
+}
+```
+
+<br>
+
+💡**상세보기에서 `N/A`로 뜨는 데이터를 `no Data`로 변경**
+
+```jsx
+const result = await url.json();
+Object.entries(result).forEach(([key, value]) => {
+  if (value === "N/A") {
+    result[key] = "no Data";
+    return;
+  }
+});
+```
+
+<br>
+
+## ⚠️Issue
+
+**⚠️새로운 키워드로 검색했을 때 기존 메인에서 이미 가져온 카드리스트에서 뒤에 추가됨. 이전 카드리스트를 없애야 함.**
+
+```jsx
+async function cardList(pageNo, init = false) {
+  if (init) {
+    cards.innerHTML = movieCard.join("\n");
+    return;
+  }
+  cards.innerHTML += movieCard.join("\n");
+}
+--------------------------------------------------
+// 새로운 페이지 부를때
+pageNo = 1;
+cardList(pageNo, true);
+
+// 기존 페이지에 카드만 늘릴때
+pageNo += 1;
+cardList(pageNo);  // 기본값이 false이므로 생략
+```
+
+<br>
+
+**⚠️한국어로 검색시, 검색 결과가 하나도 없음 → 영어로만 입력 받아야함**
+
+```jsx
+<input type='text' style="ime-mode:disabled;">  // 영어만 가능
+<input type='text' style="ime-mode:active;">  // 한글 우선, 영어 가능
+<input type='text' style="ime-mode:inactive;">  // 영어 우선, 한글 가능
+```
+
+```jsx
+const searchInput = document.getElementById("input");
+searchInput.addEventListener("keyup", (event) => {
+  const key = event.key || event.keyCode;
+
+  if (key === "Process") {
+    alert("영어만 입력 가능합니다.");
+    searchInput.value = null;
+    return;
+  }
+  event.preventDefault();
+});
+```
+
+<br>
+
+**⚠️새로운 검색어를 입력했을 때, 스크롤 위치가 이전 검색화면과 동일. 스크롤 위치를 맨 위로 초기화 해야함**
+
+```jsx
+window.scrollTo(0, 0);
+```
+
+<br>
+
+**⚠️영화 제목의 길이가 길면 화면 밖으로 밀려남. 일정 길이 이상은 말 줄임표 사용**
+
+```scss
+@mixin ellipsis($line-cnt, $line-height) {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: $line-cnt; /* 라인수 */
+  -webkit-box-orient: vertical;
+  word-break: keep-all;
+  word-wrap: break-word;
+  line-height: $line-height;
+  height: $line-height * $line-cnt;
+}
+```
+
+<br>
+
+⚠️**모달이 떠있을 때 window 스크롤 이벤트 방지**
+
+```jsx
+function preventScroll(e) {
+  e.preventDefault();
+  e.stopPropagation();
+
+  return false;
+}
+
+function disableScrollWheel() {
+  window.addEventListener("wheel", preventScroll, { passive: false });
+}
+
+function enableScrollWheel() {
+  window.removeEventListener("wheel", preventScroll);
+}
+```
--- js/main.js
+++ js/main.js
@@ -41,6 +41,7 @@ function defaultImg(imdbID, title, year) {
   return card;
 }
 
+// 로고 눌렀을 때 초기 메인으로 돌아가기
 function goHome() {
   const mainTitle = document.querySelector(".movie-searcher-title");
   const mainHeader = document.querySelector("#main-header");
@@ -58,25 +59,37 @@ function goHome() {
 /**
  * 1. getMovies를 통해서 전체 데이터 받아오기
  * 2. getMovies의  결과 데이터를 map으로 순회하면서 createCard 호출
+ * 3. 포스터가 없는 영화의 이미지에는 디폴트 이미지 띄우기
  */
 async function cardList(pageNo, init = false) {
-  // id가 input인 태그를 찾아서 value값을 변수에 저장한다.
+  // id가 input인 태그를 찾아서 value값을 변수에 저장
   const input = document.getElementById("input");
   const title = input.value;
+  const cards = document.getElementById("cards");
+  const totalSearch = document.querySelector(".total-search");
   const data = await getMovies(title, pageNo);
+  console.log(data.Response);
+  if (data.Response === "False") {
+    totalSearch.innerHTML = `" <strong>${title}</strong> "의 검색결과: 0개`;
+    cards.innerHTML =
+      "<img class='no-result' src='image/no-results.png' alt='no-results'>";
+    return;
+  }
+
   const movieCard = data.Search.map((d) => {
     if (d.Poster !== "N/A") {
+      // 큰 포스터 가져오기
       const bigPoster = d.Poster.replace("SX300", "SX700");
       return createCard(d.imdbID, d.Title, d.Year, bigPoster);
     } else {
+      // 포스터가 없는 영화 정보
       return defaultImg(d.imdbID, d.Title, d.Year);
     }
   });
-  const totalSearch = document.querySelector(".total-search");
-  const totalSearchData = data.totalResults;
 
-  const cards = document.getElementById("cards");
+  const totalSearchData = data.totalResults ? data.totalResults : "0";
 
+  // 총 검색결과 수
   if (init) {
     totalSearch.innerHTML = `" <strong>${title}</strong> "의 검색결과: ${totalSearchData}개`;
     cards.innerHTML = movieCard.join("\n");
@@ -86,8 +99,22 @@ async function cardList(pageNo, init = false) {
   cards.innerHTML += movieCard.join("\n");
 }
 
+// Loading Image
+function activateLoader() {
+  const loader = document.getElementById("loader");
+  loader.className = "loader";
+}
+
+function deactivateLoader() {
+  const loader = document.getElementById("loader");
+  loader.className = "";
+}
+
+// 초기 화면을 바꾸고 카드 리스트 만들기 + 스크롤 했을 때 추가 카드 만들기
 function initialize() {
   const searchInput = document.getElementById("input");
+
+  // 한글 입력 방지
   searchInput.addEventListener("keyup", (event) => {
     const key = event.key || event.keyCode;
 
@@ -101,25 +128,42 @@ function initialize() {
 
   const searchForm = document.querySelector(".search-container");
   searchForm.addEventListener("submit", (e) => {
+    // loader on
+    activateLoader();
+
     const headerClass = document.querySelector("header").classList;
     if (headerClass.contains("init") && searchInput.value !== "") {
+      // form의 디폴트 이벤트(새로고침) 방지
       e.preventDefault();
       headerClass.replace("init", "result");
       pageNo = 1;
-      cardList(pageNo, true).then(() => clickPopup());
+
+      // 카드 리스트가 만들어진 뒤, 바로 modal을 만드는 함수 실행
+      cardList(pageNo, true).then(() => {
+        deactivateLoader();
+        clickPopup();
+      });
       goHome();
       return;
     }
+    // 새로운 검색어 입력했을 때, 스크롤을 상단으로 초기화
     window.scrollTo(0, 0);
+    const cards = document.querySelector("#cards");
+    cards.innerHTML = "";
     e.preventDefault();
     pageNo = 1;
-    cardList(pageNo, true).then(() => clickPopup());
+    cardList(pageNo, true).then(() => {
+      deactivateLoader();
+      clickPopup();
+    });
   });
 
+  // 초기 화면에서 검색창 누르면 Welcome 문구 사라지게 만들기
   searchInput.addEventListener("click", () => {
     searchInput.innerText = "";
   });
 
+  // Lodash를 이용한 무한 스크롤
   document.addEventListener(
     "scroll",
     _.throttle(() => {
@@ -141,9 +185,18 @@ async function getDetail(imdbID) {
   const url = await fetch(
     `https://www.omdbapi.com?apikey=${apiKey}&i=${imdbID}`
   );
-  const result = url.json();
+  const result = await url.json();
+  Object.entries(result).forEach(([key, value]) => {
+    if (value === "N/A") {
+      result[key] = "no Data";
+      return;
+    }
+  });
+
   return result;
 }
+
+// 카드 MODAL 만들기
 async function cardDetail(imdbID) {
   const detailPoster = document.querySelector(".detail__poster");
   const detailTitle = document.querySelector(".detail__title");
@@ -167,7 +220,7 @@ async function cardDetail(imdbID) {
   detailDirector.innerText = `Director: ${data.Director}`;
   detailActors.innerText = `Actors: ${data.Actors}`;
   detailPlot.innerText = `${data.Plot}`;
-  if (data.Poster !== "N/A") {
+  if (data.Poster !== "no Data") {
     const bigPoster = data.Poster.replace("SX300", "SX700");
     detailPoster.src = bigPoster;
     detailPoster.alt = data.Title;
@@ -179,6 +232,7 @@ async function cardDetail(imdbID) {
   }
 }
 
+// Modal창이 떠 있을 때 배경 스크롤 방지
 function preventScroll(e) {
   e.preventDefault();
   e.stopPropagation();
@@ -193,31 +247,62 @@ function disableScrollWheel() {
 function enableScrollWheel() {
   window.removeEventListener("wheel", preventScroll, { passive: true });
 }
+
+/**
+ * 이벤트 리스너의 최적화를 위해 카드 리스트를 클릭했을 때 팝업이 뜨도록 하고
+ * 이벤트가 일어난 타겟 중 가장 가까운 요소의 정보를 가져오도록 함
+ */
 function clickPopup() {
   const movieCards = document.querySelector("#cards");
   movieCards.addEventListener("click", (e) => {
+    activateLoader();
     const popup = document.querySelector(".dark-window");
     const movieCard = e.target.closest(".movie-card");
     const movieID = movieCard.id;
+    console.log(movieID);
 
+    // 영화 카드가 아니라면 아무 이벤트 일어나지 않음
     if (!movieCard) return;
 
+    // 카드 리스트 중 영화카드가 아니라면 아무 이벤트 일어나지 않음
     if (!movieCards.contains(movieCard)) return;
 
     popup.classList.add("show");
+
+    // 배경 스크롤 방지
     disableScrollWheel();
-    cardDetail(movieID);
+
+    // 클릭된 카드의 omdbID를 통해 영화 상세 정보 가져오기
+    cardDetail(movieID).then(() => deactivateLoader());
+    e.preventDefault();
   });
 }
+
 function closePopup() {
-  const closeBtn = document.querySelector(".close");
-  closeBtn.addEventListener("click", () => {
-    const popup = document.querySelector(".dark-window");
-    const moviePoster = document.querySelector(".detail__poster");
-    popup.classList.remove("show");
-    moviePoster.src = "";
-    enableScrollWheel();
-  });
+  const popup = document.querySelector(".dark-window");
+  const moviePoster = document.querySelector(".detail__poster");
+
+  // modal창 안보이게 하기
+  popup.classList.remove("show");
+
+  // modal창의 포스터 사진 비우기. 비우지 않으면 다음 카드 modal에서 사진이 남음
+  moviePoster.src = "";
+
+  // 다시 스크롤 가능
+  enableScrollWheel();
 }
+
+// X버튼 눌렀을 때 모달 끄기
+const closeBtn = document.querySelector(".close");
+closeBtn.addEventListener("click", closePopup);
+
+// 모달 영역 외의 화면 클릭시 모달 끄기
+const darkWindow = document.querySelector(".dark-window");
+darkWindow.addEventListener("click", (e) => {
+  const modal = document.querySelector(".card__popup");
+  if (e.target !== darkWindow) return;
+  if (e.target === modal) return;
+  closePopup();
+});
 
 initialize();
